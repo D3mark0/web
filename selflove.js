f0aeda1e959ba0e69030d9c7df1996d7425f13e3
Alexander Mikhailov <striown@gmail.com>
2019-08-22 00:44:44 +0700

    add modal to selflove page

---
+++ selflove.js
@@ -0,0 +1,9 @@
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+  document.body.style.overflowY = "hidden";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+  document.body.style.overflowY = "scroll";
+}
