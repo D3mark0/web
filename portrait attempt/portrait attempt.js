f075cb2ff257499db9dd829d76761aa7832b875a
Alexander Mikhailov <striown@gmail.com>
2020-05-01 17:50:15 +0700

    add text blocks to portrait attempt's main page

---
+++ portrait attempt/portrait attempt.js
@@ -0,0 +1,15 @@
+function hideAll() {
+  document.getElementById('title').style.display = "none";
+  document.getElementById('modal1').style.display = "none";
+  document.getElementById('modal2').style.display = "none";
+}
+
+function openModal(modal) {
+  hideAll();
+  document.getElementById(modal).style.display = "block";
+}
+
+function closeModal(modal) {
+  document.getElementById(modal).style.display = "none";
+  document.getElementById('title').style.display = "block";
+}
