3f6a481a2cf33a544295e74a25d81d405c4cc4f9
Alexander Mikhailov <striown@gmail.com>
2020-07-25 20:18:51 +0700

    add functionality to stumps

---
+++ stumps/stumps.js
@@ -0,0 +1,153 @@
+var myImages1 = new Array();
+myImages1.push("../img/stumps/stump-1.jpg");
+myImages1.push("../img/stumps/stump-2.jpg");
+myImages1.push("../img/stumps/stump-3.jpg");
+myImages1.push("../img/stumps/stump-4.jpg");
+myImages1.push("../img/stumps/stump-5.jpg");
+myImages1.push("../img/stumps/stump-6.jpg");
+myImages1.push("../img/stumps/stump-7.jpg");
+myImages1.push("../img/stumps/stump-8.jpg");
+myImages1.push("../img/stumps/stump-9.jpg");
+myImages1.push("../img/stumps/stump-10.jpg");
+
+myImages1.push("../img/stumps/stump-11.jpg");
+myImages1.push("../img/stumps/stump-12.jpg");
+myImages1.push("../img/stumps/stump-13.jpg");
+myImages1.push("../img/stumps/stump-14.jpg");
+myImages1.push("../img/stumps/stump-15.jpg");
+myImages1.push("../img/stumps/stump-16.jpg");
+myImages1.push("../img/stumps/stump-17.jpg");
+myImages1.push("../img/stumps/stump-18.jpg");
+myImages1.push("../img/stumps/stump-19.jpg");
+myImages1.push("../img/stumps/stump-20.jpg");
+
+myImages1.push("../img/stumps/stump-21.jpg");
+myImages1.push("../img/stumps/stump-22.jpg");
+myImages1.push("../img/stumps/stump-23.jpg");
+myImages1.push("../img/stumps/stump-24.jpg");
+myImages1.push("../img/stumps/stump-25.jpg");
+myImages1.push("../img/stumps/stump-26.jpg");
+myImages1.push("../img/stumps/stump-27.jpg");
+myImages1.push("../img/stumps/stump-28.jpg");
+myImages1.push("../img/stumps/stump-29.jpg");
+myImages1.push("../img/stumps/stump-30.jpg");
+
+myImages1.push("../img/stumps/stump-31.jpg");
+myImages1.push("../img/stumps/stump-32.jpg");
+myImages1.push("../img/stumps/stump-33.jpg");
+myImages1.push("../img/stumps/stump-34.jpg");
+myImages1.push("../img/stumps/stump-35.jpg");
+myImages1.push("../img/stumps/stump-36.jpg");
+myImages1.push("../img/stumps/stump-37.jpg");
+myImages1.push("../img/stumps/stump-38.jpg");
+myImages1.push("../img/stumps/stump-39.jpg");
+myImages1.push("../img/stumps/stump-40.jpg");
+
+myImages1.push("../img/stumps/stump-41.jpg");
+myImages1.push("../img/stumps/stump-42.jpg");
+myImages1.push("../img/stumps/stump-43.jpg");
+myImages1.push("../img/stumps/stump-44.jpg");
+myImages1.push("../img/stumps/stump-45.jpg");
+myImages1.push("../img/stumps/stump-46.jpg");
+myImages1.push("../img/stumps/stump-47.jpg");
+myImages1.push("../img/stumps/stump-48.jpg");
+myImages1.push("../img/stumps/stump-49.jpg");
+myImages1.push("../img/stumps/stump-50.jpg");
+
+myImages1.push("../img/stumps/stump-51.jpg");
+myImages1.push("../img/stumps/stump-52.jpg");
+myImages1.push("../img/stumps/stump-53.jpg");
+myImages1.push("../img/stumps/stump-54.jpg");
+myImages1.push("../img/stumps/stump-55.jpg");
+myImages1.push("../img/stumps/stump-56.jpg");
+myImages1.push("../img/stumps/stump-57.jpg");
+myImages1.push("../img/stumps/stump-58.jpg");
+myImages1.push("../img/stumps/stump-59.jpg");
+myImages1.push("../img/stumps/stump-60.jpg");
+
+myImages1.push("../img/stumps/stump-61.jpg");
+myImages1.push("../img/stumps/stump-62.jpg");
+myImages1.push("../img/stumps/stump-63.jpg");
+myImages1.push("../img/stumps/stump-64.jpg");
+myImages1.push("../img/stumps/stump-65.jpg");
+myImages1.push("../img/stumps/stump-66.jpg");
+myImages1.push("../img/stumps/stump-67.jpg");
+myImages1.push("../img/stumps/stump-68.jpg");
+myImages1.push("../img/stumps/stump-69.jpg");
+myImages1.push("../img/stumps/stump-70.jpg");
+
+myImages1.push("../img/stumps/stump-71.jpg");
+myImages1.push("../img/stumps/stump-72.jpg");
+myImages1.push("../img/stumps/stump-73.jpg");
+myImages1.push("../img/stumps/stump-74.jpg");
+myImages1.push("../img/stumps/stump-75.jpg");
+myImages1.push("../img/stumps/stump-76.jpg");
+myImages1.push("../img/stumps/stump-77.jpg");
+myImages1.push("../img/stumps/stump-78.jpg");
+myImages1.push("../img/stumps/stump-79.jpg");
+myImages1.push("../img/stumps/stump-80.jpg");
+
+myImages1.push("../img/stumps/stump-81.jpg");
+myImages1.push("../img/stumps/stump-82.jpg");
+myImages1.push("../img/stumps/stump-83.jpg");
+myImages1.push("../img/stumps/stump-84.jpg");
+myImages1.push("../img/stumps/stump-85.jpg");
+myImages1.push("../img/stumps/stump-86.jpg");
+myImages1.push("../img/stumps/stump-87.jpg");
+myImages1.push("../img/stumps/stump-88.jpg");
+myImages1.push("../img/stumps/stump-89.jpg");
+myImages1.push("../img/stumps/stump-90.jpg");
+
+var myNotes1 = new Array();
+
+myNotes1.push("Штирлиц пошел собирать грибы. Глянул под одного дерево — ничего нет. Глянул под другое — тоже нет. — Да, не сезон, — подумал Штирлиц и сел в сугроб.")
+myNotes1.push("Жена мужу: — Зай, я не сдала на права. Муж: — В чем дело? Что завалила? Теорию? Практику?Жена: — Инспектора, дерево и двух бомжей.")
+myNotes1.push("Начальник приехал в глубинку: — Как будем с кризисом бороться? — обращается он к собравшимся. — Я вот взял поросеночка. — отвечает один мужик. — Буду откармливать его на мясо. — Правильно сделали! А вы что решили? — обращается он к другому. — А я решил посадить дерево. Буду откармливать его на дрова...")
+myNotes1.push("В Греции найдено дерево, чей возраст достигает 3000 лет. По свидетельству ученых, если бы дерево могло говорить, у него был бы рот.")
+myNotes1.push("Залез пацан в сад, сидит на дереве, жрёт яблоки. Внизу сторож: — Не боись, пацан, хошь тебе анекдот расскажу? Рассказал. Пацан долго смеялся, упал, сломал шею. Сторож, ставя ногу на труп и потирая руки: — Двадцатый...")
+myNotes1.push("— А почему дерево упало? — Оно что, без сознания?")
+myNotes1.push("Съешь бобра — спаси дерево!")
+myNotes1.push("— Пни! — Кого пнуть?! — Вы оба — пни!")
+myNotes1.push("На дерево лезет черепашка, залезает на самую верхушку, прыгает оттуда, отчаянно машет лапками, падает на землю, ушибается, но вновь и вновь лезет обратно. За этим наблюдают две птицы, и одна говорит другой: — Может, сказать нашему ребёнку, что он приёмный?")
+myNotes1.push("Дятел—мазохист долбит деревья вдоль.")
+
+myNotes1.push("Корова лезет на дерево. Ворона ей: — Корова, ты чего это на дерево лезешь? — Яблок хочу поесть. — Так это же елка! — А у меня с собой...")
+myNotes1.push("Горнолыжный инструктор — своим подопечным: — И запомните, ребята, главное: — Дерево лучше всего объезжать с одной стороны!")
+myNotes1.push("Сидит ворона с сыром во рту на дереве. Пробегает мимо лиса с палкой, бьёт ворону, забирает сыр и убегает. Ворона с удивлением: — Ну, ничего себе басню сократили!..")
+myNotes1.push("Грызут два короеда деревяшку. Один другому: — Ну и что? Обыкновенное дерево! А ты все: — Страдивари, Страдивари...")
+myNotes1.push("— Ты знаешь, что твой батя убил двух зайцев одним махом? — И сына вырастил, и дерево?")
+myNotes1.push("Беседуют два философа: — Кто мы такие? Так, листочки на древе жизни. — Не скажите, коллега. Мне порой кажется, что наше древо жизни — хвойное дерево, уж больно много на нем разных шишек.")
+myNotes1.push("— Хорошо клюет в этом озере? — Клюет?! Я должен прятаться за дерево, чтоб рыбы меня не разорвали, когда я насаживаю червяка на крючок.")
+myNotes1.push("— Папа, а почему про одного человека говорят, что он достиг чего—то, а про другого — добился? — Ну вот смотри, сынок. Если ты увидишь высоко на дереве яблоко, полезешь за ним и сорвёшь его, то ты его достиг. А если, отлупив младшего брата, отобрал у него яблоко — значит, добился.")
+myNotes1.push("— Мам, дай денег на ноут. — Нет! Деньги, по—твоему что, на деревьях растут?! — Мам, а из чего сделаны деньги? — Из бумаги, — задумчиво. — А бумага из чего? — ...")
+myNotes1.push("— А под берёзой могут расти подосиновики? — Могут,если они в деревьях не разбираются.")
+
+myNotes1.push("Дистрофик гуляет по лесу. Вдруг с дерева срывается лист и падает ему на голову. Несчастный в обмороке. Очнувшись, стонет: — Все листья как листья, а этот — ну, как утюг!")
+myNotes1.push("В секции выжигания по дереву требуется преподаватель, ученики и смысл.")
+myNotes1.push("Сын читает, отец занимается своими делами. — Папа, кто такой филин? — Рыба такая, не мешай! — А почему написано, что он сидит на дереве? – Откуда я знаю? Может он сумасшедший.")
+myNotes1.push("— Могу я предложить вам кипяток, пропущенный через семена дерева? — С жидкостью, которой кормит детёнышей другой биологический вид, пожалуйста.")
+myNotes1.push("Глубокая зима... Ходит медведь по лесу и возле каждого дерева останавливается и бьется головой об дерево: — Ну зачем же я выпил столько кофе...")
+myNotes1.push("Крокодил — сидя на дереве, и тупо вглядываясь в ландшафт: — Говорили же родители — не есть наркоманов...")
+myNotes1.push("Идет прохожий по улице, видит — на дереве «Запорожец» висит, а под деревом его водитель стоит. Подходит и спрашивает: — А че это твой Запор там делает? — Ну всего я от него ожидал, но чтобы он еще и собак боялся...")
+myNotes1.push("Мать говорит дочери: — Дура, что повела его в кино! — Там каждая лучше тебя! Ты поведи в парк — там одни деревья!")
+myNotes1.push("Во время экзамена по вождению машина врезается в дерево. Экзаменатор стряхивает с себя осколки стекла и спокойно говорит: — Так, а теперь покажите, как вы останавливаете машину, когда перед вами нет препятствия.")
+myNotes1.push("Жидкость для снятия котят с дерева.")
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function pick() {
+  document.getElementById("img").src = myImages1[getRandomInt(0, myImages1.length - 1)];
+  document.getElementById("text").innerHTML = myNotes1[getRandomInt(0, myNotes1.length - 1)];
+}
+
+function showdescription() {
+  document.getElementById('body').style.display = 'none';
+  document.getElementById('descriptrion').style.display = 'block';
+}
+
+function showbody() {
+  document.getElementById('descriptrion').style.display = 'none';
+  document.getElementById('body').style.display = 'block';
+}
